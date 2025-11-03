0df404ca3f9aa5c1e498f2e834482661d658c689
vivekexceed-automation <63901762+vivekexceed@users.noreply.github.com>
2025-11-03 20:12:29 +0530

    align the code

--- src/main/java/com/qa/practice/pages/DashboardPg.java
+++ src/main/java/com/qa/practice/pages/DashboardPg.java
@@ -3,18 +3,17 @@ package com.qa.practice.pages;
 import org.testng.annotations.Test;
 
 public class DashboardPg {
-	
+
 	@Test
 	public void dashboardPgTest() {
 		System.out.println("Checking Login Page");
 	}
-	
+
 	@Test
 	public void TitleTest() {
 		System.out.println("Checking Title");
 	}
-	
-	
+
 	@Test(priority = Integer.MAX_VALUE)
 	public void urlTest() {
 		System.out.println("Checking Url");
