fbf6b2cdaa7eeb470b708e53c7ec12224e9d3a44
vivekexceed-automation <63901762+vivekexceed@users.noreply.github.com>
2025-11-03 15:12:07 +0530

    Vivek added Login Page

---
+++ src/main/java/com/qa/practice/pages/LoginPg.java
@@ -0,0 +1,11 @@
+package com.qa.practice.pages;
+
+import org.testng.annotations.Test;
+
+public class LoginPg {
+
+	@Test
+	public void LoginTest() {
+		System.out.println("Checking Login Page");
+	}
+}
