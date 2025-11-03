909c46321bbc546ac9abb049e94c5a2c74f35045
vivekexceed-automation <63901762+vivekexceed@users.noreply.github.com>
2025-11-03 16:21:36 +0530

    Vivek added dashboard page

---
+++ src/main/java/com/qa/practice/pages/DashboardPg.java
@@ -0,0 +1,11 @@
+package com.qa.practice.pages;
+
+import org.testng.annotations.Test;
+
+public class DashboardPg {
+	
+	@Test
+	public void dashboardPg() {
+		System.out.println("Checking Login Page");
+	}
+}
