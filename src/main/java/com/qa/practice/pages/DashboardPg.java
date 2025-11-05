b4aa1826c5c59d021ae910f6f9a4c37e4be89893
vivekexceed-automation <63901762+vivekexceed@users.noreply.github.com>
2025-11-05 12:09:44 +0530

    updated DashboardPg as per review by Vivek please check again and approve the PR

--- src/main/java/com/qa/practice/pages/DashboardPg.java
+++ src/main/java/com/qa/practice/pages/DashboardPg.java
@@ -2,7 +2,7 @@ package com.qa.practice.pages;
 
 public class DashboardPg {
 
-	public void accontsPg() {
-		System.out.println("Checking Login Page");
+	public void dashboardPg() {
+		System.out.println("Checking Dashboard Page");
 	}
 }
