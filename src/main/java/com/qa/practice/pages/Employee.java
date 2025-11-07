a23dcccf164477c962a9468a3c07603dc160b032
vivekexceed-automation <63901762+vivekexceed@users.noreply.github.com>
2025-11-07 18:20:36 +0530

    Added String empEclipseTxt variable in Employee class

--- src/main/java/com/qa/practice/pages/Employee.java
+++ src/main/java/com/qa/practice/pages/Employee.java
@@ -3,6 +3,9 @@ package com.qa.practice.pages;
 public class Employee {
 
 	int empEclipse = 5;
+	
+	String empEclipseTxt = "Vivek Emp"; 
+	
 
 	public void empEclipse() {
 		System.out.println("Hello Emp Eclipse");
